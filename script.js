a7951e9efcc4cb1a4cd7ad576d588d95a7cba333
jacquesdouglasdev <jacquesdouglasdev@gmail.com>
2023-06-17 14:44:49 -0300

    adicionado constante no arquivo js

---
+++ script.js
@@ -0,0 +1,6 @@
+//Parte 1
+const numero = document.querySelector(".numero-principal");
+const botaoMenos = document.querySelector(".botao.decremento");
+const botaoMais = document.querySelector(".botao.incremento");
+const botaoReset = document.querySelector(".botao.reset");
+
