29924e3f97142a050176ffcc52e3f2f02d63138b
jacquesdouglasdev <jacquesdouglasdev@gmail.com>
2023-06-17 14:44:49 -0300

    adicionado constante no arquivo js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const botaoMenos = document.querySelector(".botao.decremento");
 const botaoMais = document.querySelector(".botao.incremento");
 const botaoReset = document.querySelector(".botao.reset");
 
+//parte 2
+botaoMais.addEventListener("click", function () {
+	numero.innerHTML = +numero.innerHTML + 1;
+});
+botaoMenos.addEventListener("click", function () {
+	numero.innerHTML = +numero.innerHTML - 1;
+});
+botaoReset.addEventListener("click", function () {
+	numero.innerHTML = 0;
+});
